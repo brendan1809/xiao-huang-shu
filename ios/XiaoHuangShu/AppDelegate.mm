f3a61ac5f37a8ce7b36494f1111340dd250778c9
brendan1809 <brendan.lim@symple.my>
2023-10-23 17:49:49 +0800

    chore: install and setup rn firebase

--- ios/XiaoHuangShu/AppDelegate.mm
+++ ios/XiaoHuangShu/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import "Firebase.h"
 #import "RNBootSplash.h"
 
 #import <React/RCTBundleURLProvider.h>
@@ -8,11 +9,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
   self.moduleName = @"XiaoHuangShu";
 
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+  [FIRApp configure];
 
   [super application:application didFinishLaunchingWithOptions:launchOptions];
 

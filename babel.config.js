8f7c45231a68d965f45cdd5dc058e6a928840a0b
brendan1809 <brendan.lim@symple.my>
2023-10-26 15:40:03 +0800

    chore: update babel config

--- babel.config.js
+++ babel.config.js
@@ -26,6 +26,28 @@ const plugins = [
   ],
   /** react-native-reanimated web support @see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#web */
   "@babel/plugin-proposal-export-namespace-from",
+
+  [
+    "module-resolver",
+    {
+      cwd: "./",
+      root: ["./app"],
+      alias: {
+        assets: "./assets",
+        components: "./app/components",
+        containers: "./app/containers",
+        i18n: "./app/i18n",
+        config: "./app/config",
+        utils: "./app/utils",
+        models: "./app/models",
+        screens: "./app/screens",
+        theme: "./app/theme",
+        colors: "./app/theme/colors",
+      },
+      extensions: [".ts", ".tsx"],
+    },
+  ],
+
   /** NOTE: This must be last in the plugins @see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#babel-plugin */
   "react-native-reanimated/plugin",
 ]
